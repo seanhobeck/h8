ba617e5740097525058883323f0ed476941730d7
sicily <121409961+seanhobeck@users.noreply.github.com>
2023-08-25 23:22:22 -0400

    updates to the emulator
    - lost a lot of data due to git :(

--- src/emu/emu.c
+++ src/emu/emu.c
@@ -4,12 +4,16 @@
  */
 #include "emu.h"
 
+/// @note: For readibility.
+#define strcmp(str, cmp, ret) if (!strstr(str, cmp)) \
+                                return ret;
+
 
 
 /*                  @section: Data                     */
 
 /// @note: The emulators memory.
-extern unsigned char memory[65536];
+extern unsigned char memory[0x7FFF];
 
 /*            @section: Utility Functions              */
 
@@ -17,10 +21,29 @@ extern unsigned char memory[65536];
 void load_memd(unsigned short loc, unsigned char data) { assert(memory != 0); memory[loc] = data; };
 /// @note: Getting the data a the memory location.
 unsigned char get_memd(unsigned short loc) { assert(memory != 0); return memory[loc]; };
+/// @note: Getting the instruction name from a string.
+signed get_inst(const char* p_str)
+{
+    /// String check.
+    assert(p_str != 0);
+
+    /// All of the instructions.
+    strcmp(p_str, "mov", MOV); strcmp(p_str, "lda", LDA);
+    strcmp(p_str, "add", ADD); strcmp(p_str, "sub", SUB);
+    strcmp(p_str, "cmp", CMP); strcmp(p_str, "jc", JC);
+    strcmp(p_str, "or", OR); strcmp(p_str, "not", NOT);
+    strcmp(p_str, "and", AND);
+    strcmp(p_str, "hlt", HLT); strcmp(p_str, "out", OUT);
+    return -1;
+};
 
 /*            @section: External Functions             */
 
-extern void emulate_data(struct node_t* list)
+extern void emulate_data(const struct node_t list)
 {
+    /// The returned instruction tree.
+    struct instruction_t r;
     
+    /// Getting the iterator.
+    struct node_t it = list;
 };
--- src/emu/emu.h
+++ src/emu/emu.h
@@ -9,30 +9,18 @@
 
 
 /// @note: enumerator for all of the instructions.
-enum e_instruction : int
-{
-  MOV = 0x0,
-  LDA,ADD,SUB,
-  CMP,OR,NOT,
-  AND,JC,HLT,
-  OPT
-};
+enum e_instruction : int { MOV = 0x0, LDA,ADD,SUB,CMP,OR,NOT,AND,JC,HLT,OUT };
 
 /// @note: enumerator for all of the registers.
-enum e_register : int
-{
-  R1 = 0x0,
-  R2,R3,
-  R4,R5,
-  AX,IP,
-};
+enum e_register : int { R1 = 0x0,R2,R3,R4,R5,AX,IP };
 
 /// @note: enumerator for all of the flags.
-enum e_flag : int
+enum e_flag : int { ZF = 0x0,CF,GF,LF,SF };
+
+/// @note: Structure for all of the grouping of each instruction.
+typedef struct instruction_t
 {
-  ZF = 0x0,
-  CF,GF,
-  LF,SF
+    char** p_data;
 };
 
 /*          @section: Memory-based Functions           */
@@ -41,8 +29,10 @@ enum e_flag : int
 void load_memd(unsigned short loc, unsigned char data);
 /// @note: Getting the data from a memory location.
 unsigned char get_memd(unsigned short loc);
+/// @note: Getting the instruction from a string.
+signed get_inst(const char* p_str);
 
 /*            @section: External Functions             */
 
 /// @note: Emulating all of the data in the linked list.
-extern void emulate_data(struct node_t* list);
+extern void emulate_data(const struct node_t list);
